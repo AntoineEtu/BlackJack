61f4a9176d3b2262009d3ec7662ca2e6b375a827
Florimond JOUFFROY <florimond.jouffroy@gmail.com>
2017-02-11 21:38:58 +0100

    casino

--- BlackJack/ViewModel/CasinoViewModel.cs
+++ BlackJack/ViewModel/CasinoViewModel.cs
@@ -5,8 +5,11 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
@@ -37,6 +40,46 @@ namespace BlackJack.ViewModel
             }
         }
 
+
+        // définition de la command déconexion
+        private ICommand deconexionUtilisateur;
+        public ICommand DeconexionUtilisateur
+        {
+            get
+            {
+                if (deconexionUtilisateur == null)
+                {
+                    deconexionUtilisateur = deconexionUtilisateur ?? (deconexionUtilisateur = new RelayCommand(obj => { Deconexion(); }));
+                }
+                return deconexionUtilisateur;
+            }
+        }
+
+        public async void Deconexion()
+        {
+
+        }
+        public async void UserDeconexion(User user)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://demo.comte.re");
+
+                var json = JsonConvert.SerializeObject(user.email);
+                var itemJson = new StringContent(json, Encoding.UTF8, "application/json");
+                HttpResponseMessage response = await client.PostAsync("/api/auth/logout", itemJson);
+                if (response.IsSuccessStatusCode)
+                {
+                    var message = new MessageDialog("Vous avez été déconnecté");
+                    await message.ShowAsync();
+
+
+                    actualFrame.Navigate(typeof(MainPage));
+
+
+                }
+            }
+        }
 
     }
 }
